4407aa67b3de0c23787f13ff7e221fb5d9eb29ff
adamtornkvist <adamtornqvist23@gmail.com>
2021-03-15 21:16:18 +0100

    Lab 6 typ klar

--- lab6/STATIONAR.m
+++ lab6/STATIONAR.m
@@ -1,12 +1,28 @@
 function markov = STATIONAR(m)
 
-zeroed = m >= 0;
-if ~any(zeroed == 0) && sum(m) == 1 %Behövs mer test
-    % Ge den stationära fördelningen
+
+zeroed = m >= 0
+summa = sum(m')
+allzero = all(zeroed == 1)
+~(all(summa > 0.99) && (all(summa < 1.01)))
+~all(allzero' == 1)
+
+m
+matrix = m^1000
+markovMaybe = matrix(1,:)'
+newSumma = sum(markovMaybe)
+m*markovMaybe
+
+
+
+if ~all(allzero' == 1) || ~(all(summa > 0.99) && (all(summa < 1.01)))
+    fprintf('FELAKTIG MATRIS ');
     
-elseif 3 > 2
-    fprintf('KONVERGERAR EJ TILL STATIONÄR FÖRDELNING')
+elseif ~(m*markovMaybe == markovMaybe) 
+    % (m^t - i) * x = 0 <--- Är det bättre att använda det?
+    fprintf('KONVERGERAR EJ TILL STATIONÄR FÖRDELNING ')
     
-elseif  4 > 3
-    fprintf('FELAKTIG MATRIS');
+else
+    
+    markov = markovMaybe;
 end
--- lab6/lab6.m
+++ lab6/lab6.m
@@ -1,16 +1,26 @@
 %% Lab 6
 clc, clear
 A = [1 2 1; 4 5 6];
-
+B = [1/3 1/3 1/3; 1 0 0];
 P = 0;
 
-zeroed = A >= P
 
+markov = [1/6 2/6 3/6; 1 0 0; 3/6 2/6 1/6]
+testing = [0.9 0.1; 0.5 0.5]
+errormarkov = [1 1 1; 0 1 0; 0 0 0];
+wrongmarkov = [1/3 1/3 1/3; 0 1 0; 0 0 1;];
+zeroed = A >= P;
+summa = sum(A');
+test = all(summa == 1);
+any(zeroed == 0);
 
-any(zeroed == 0)
+%fprintf('\n Rätt: \n')
+STATIONAR(markov)
 
-if ~any(zeroed == 0)
-    fprintf('TRUUUE')
-   
-end
+%fprintf('\n \n Error: \n \n')
+%STATIONAR(errormarkov)
 
+%fprintf('\n \n Fel: \n ')
+%STATIONAR(wrongmarkov)
+
+%STATIONAR(testing)
